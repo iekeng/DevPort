601ea71b5d55c29b444093ee26154a336e75bb82
iekeng <ekeng7.eb@gmail.com>
2024-02-05 17:29:00 +0100

    input field component added

--- devport-app/src/components/Education.js
+++ devport-app/src/components/Education.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import ESWFooter from './ESWFooter';
+import Container from 'react-bootstrap/Container';
 
-const Education = ({ userId, onSave }) => {
+export default function Education({ userId, onSave }) {
     const [formData, setFormData] = useState({
         institution: '',
         course: '',
@@ -73,136 +73,146 @@ const Education = ({ userId, onSave }) => {
     };
 
     return (
-        <div>
-        <main>
-            <form id="education-form" onSubmit={handleSaveEducationDetails}>
-                <section id="education-details">
-                    <div className="content-container">
-                        <div className="content">
-                            <div className="label-input-pair">
-                                <label htmlFor="institution-input" className="label">Institution</label>
-                                <input type="text" id="institution-input" className="nav-content"  name="institution" value={formData.institution} onChange={handleInputChange} />
-                            </div>
-                            <div className="label-input-pair">
-                                <label htmlFor="degree-input" className="label">Degree</label>
-                                <input type="text" id="degree-input" className="nav-content" name="degree" value={formData.degree} onChange={handleInputChange} />
-                            </div>
-                            <div className="label-input-pair">
-                                <label htmlFor="course-input" className="label">Course</label>
-                                <input type="text" id="course-input" className="nav-content" name="course" value={formData.course} onChange={handleInputChange} />
-                            </div>
-                            <div className="dates">
-                                <div className="label-input-pair">
-                                    <label htmlFor="startDate-input" className="label">Start Date</label>
-                                    <input type="date" id="startDate-input" className="nav-content" name="startDate" value={formData.startDate} onChange={handleInputChange} />
-                                </div>
-                                <div className="label-input-pair">
-                                    <label htmlFor="endDate-input" className="label">End Date</label>
-                                    <input type="date" id="endDate-input" className="nav-content" name="endDate" value={formData.endDate} onChange={handleInputChange} />
-                                </div>
-                            </div>
-                            <div className="label-input-pair">
-                                <label htmlFor="location-input" className="label">Location</label>
-                                <input type="text" id="location-input" className="nav-content" name="location" value={formData.location} onChange={handleInputChange} />
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <button type="submit" className='LSbutton' >Save</button>
+        <Container>
+        <Form>
+            <Form.Group controlId="formStep1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+            />
+            </Form.Group>
+        </Form>
+        </Container>
+        // <div>
+        // <main>
+        //     <form id="education-form" onSubmit={handleSaveEducationDetails}>
+        //         <section id="education-details">
+        //             <div className="content-container">
+        //                 <div className="content">
+        //                     <div className="label-input-pair">
+        //                         <label htmlFor="institution-input" className="label">Institution</label>
+        //                         <input type="text" id="institution-input" className="nav-content"  name="institution" value={formData.institution} onChange={handleInputChange} />
+        //                     </div>
+        //                     <div className="label-input-pair">
+        //                         <label htmlFor="degree-input" className="label">Degree</label>
+        //                         <input type="text" id="degree-input" className="nav-content" name="degree" value={formData.degree} onChange={handleInputChange} />
+        //                     </div>
+        //                     <div className="label-input-pair">
+        //                         <label htmlFor="course-input" className="label">Course</label>
+        //                         <input type="text" id="course-input" className="nav-content" name="course" value={formData.course} onChange={handleInputChange} />
+        //                     </div>
+        //                     <div className="dates">
+        //                         <div className="label-input-pair">
+        //                             <label htmlFor="startDate-input" className="label">Start Date</label>
+        //                             <input type="date" id="startDate-input" className="nav-content" name="startDate" value={formData.startDate} onChange={handleInputChange} />
+        //                         </div>
+        //                         <div className="label-input-pair">
+        //                             <label htmlFor="endDate-input" className="label">End Date</label>
+        //                             <input type="date" id="endDate-input" className="nav-content" name="endDate" value={formData.endDate} onChange={handleInputChange} />
+        //                         </div>
+        //                     </div>
+        //                     <div className="label-input-pair">
+        //                         <label htmlFor="location-input" className="label">Location</label>
+        //                         <input type="text" id="location-input" className="nav-content" name="location" value={formData.location} onChange={handleInputChange} />
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </section>
+        //         <button type="submit" className='LSbutton' >Save</button>
             
-            <section id="education-list" >
-                <section>
-                <div className="content-container">
-                    <div className="content">
-            {isAdding && (
-                        <div className="content-container">
-                            {/* Display the form for adding new education details */}
-                            <div className="content">
-                                <div className="label-input-pair">
-                                    <label className="label">Institution</label>
-                                    <input
-                                        type="text"
-                                        className="nav-content"
-                                        name="institution"
-                                        value={formData.institution}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="label-input-pair">
-                                    <label className="label">Course</label>
-                                    <input
-                                        type="text"
-                                        className="nav-content"
-                                        name="course"
-                                        value={formData.course}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                {/* <div className="label-input-pair">
-                                    <label className="label">Degree</label>
-                                    <input
-                                        type="text"
-                                        className="nav-content"
-                                        name="degree"
-                                        value={formData.degree}
-                                        onChange={handleInputChange}
-                                    />
-                                </div> */}
-                                <div className="dates">
-                                    <div className="label-input-pair">
-                                        <label className="label">Start Date</label>
-                                        <input
-                                            type="date"
-                                            className="nav-content"
-                                            name="startDate"
-                                            value={formData.startDate}
-                                            onChange={handleInputChange}
-                                        />
-                                    </div>
-                                    <div className="label-input-pair">
-                                        <label className="label">End Date</label>
-                                        <input
-                                            type="date"
-                                            className="nav-content"
-                                            name="endDate"
-                                            value={formData.endDate}
-                                            onChange={handleInputChange}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="label-input-pair">
-                                    <label className="label">Location</label>
-                                    <input
-                                        type="text"
-                                        className="nav-content"
-                                        name="location"
-                                        value={formData.location}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="add-form-buttons">
-                                <button type="submit" className='LSbutton' style={{ display: 'block', margin: '5px auto' }}>Save</button>
-                                <button type="button" className='LSbutton' onClick={cancelAddForm} style={{ display: 'block', margin: '0 auto'}}>Cancel</button>
-                            </div>
-                        </div>
-                    </div>       
-                    )}
-                    </div>
-                </div>
-                </section>
+        //     <section id="education-list" >
+        //         <section>
+        //         <div className="content-container">
+        //             <div className="content">
+        //     {isAdding && (
+        //                 <div className="content-container">
+        //                     {/* Display the form for adding new education details */}
+        //                     <div className="content">
+        //                         <div className="label-input-pair">
+        //                             <label className="label">Institution</label>
+        //                             <input
+        //                                 type="text"
+        //                                 className="nav-content"
+        //                                 name="institution"
+        //                                 value={formData.institution}
+        //                                 onChange={handleInputChange}
+        //                             />
+        //                         </div>
+        //                         <div className="label-input-pair">
+        //                             <label className="label">Course</label>
+        //                             <input
+        //                                 type="text"
+        //                                 className="nav-content"
+        //                                 name="course"
+        //                                 value={formData.course}
+        //                                 onChange={handleInputChange}
+        //                             />
+        //                         </div>
+        //                         {/* <div className="label-input-pair">
+        //                             <label className="label">Degree</label>
+        //                             <input
+        //                                 type="text"
+        //                                 className="nav-content"
+        //                                 name="degree"
+        //                                 value={formData.degree}
+        //                                 onChange={handleInputChange}
+        //                             />
+        //                         </div> */}
+        //                         <div className="dates">
+        //                             <div className="label-input-pair">
+        //                                 <label className="label">Start Date</label>
+        //                                 <input
+        //                                     type="date"
+        //                                     className="nav-content"
+        //                                     name="startDate"
+        //                                     value={formData.startDate}
+        //                                     onChange={handleInputChange}
+        //                                 />
+        //                             </div>
+        //                             <div className="label-input-pair">
+        //                                 <label className="label">End Date</label>
+        //                                 <input
+        //                                     type="date"
+        //                                     className="nav-content"
+        //                                     name="endDate"
+        //                                     value={formData.endDate}
+        //                                     onChange={handleInputChange}
+        //                                 />
+        //                             </div>
+        //                         </div>
+        //                         <div className="label-input-pair">
+        //                             <label className="label">Location</label>
+        //                             <input
+        //                                 type="text"
+        //                                 className="nav-content"
+        //                                 name="location"
+        //                                 value={formData.location}
+        //                                 onChange={handleInputChange}
+        //                             />
+        //                         </div>
+        //                         <div className="add-form-buttons">
+        //                         <button type="submit" className='LSbutton' style={{ display: 'block', margin: '5px auto' }}>Save</button>
+        //                         <button type="button" className='LSbutton' onClick={cancelAddForm} style={{ display: 'block', margin: '0 auto'}}>Cancel</button>
+        //                     </div>
+        //                 </div>
+        //             </div>       
+        //             )}
+        //             </div>
+        //         </div>
+        //         </section>
                 
-            </section>
-            </form>
-        </main>
-        {!isAdding && (
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
-                    <button type="button" id='add-button' className='LSbutton' onClick={addAnotherForm}>+</button>
-                  </div>
+        //     </section>
+        //     </form>
+        // </main>
+        // {!isAdding && (
+        //             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
+        //             <button type="button" id='add-button' className='LSbutton' onClick={addAnotherForm}>+</button>
+        //           </div>
                   
-                )}
-                <ESWFooter />
-        </div>
+        //         )}
+        // </div>
     );
 };
-
-export default Education;
